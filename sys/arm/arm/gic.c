0debc68de7a5aa3c25a07a79d84b957e9c3079c9
Michal Meloun <strejda@users.noreply.github.com>
2015-03-23 12:22:24 +0100

    Add Krit GIC to compatible strings

--- sys/arm/arm/gic.c
+++ sys/arm/arm/gic.c
@@ -173,6 +173,7 @@ static struct ofw_compat_data compat_data[] = {
 	{"arm,cortex-a7-gic",	true},
 	{"arm,arm11mp-gic",	true},
 	{"brcm,brahma-b15-gic",	true},
+	{"qcom,msm-qgic2",	true},
 	{NULL,			false}
 };
 
